d1ecaabc94e971154157bd33656b544af167745d
yurim <y1551463@gmail.com>
2025-03-19 13:40:05 +0900

    빈 목록 확인

--- spring-framework/src/main/java/com/example/spring_framework/App02HelloWorldSpring.java
+++ spring-framework/src/main/java/com/example/spring_framework/App02HelloWorldSpring.java
@@ -1,5 +1,7 @@
 package com.example.spring_framework;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App02HelloWorldSpring {
@@ -17,6 +19,8 @@ public class App02HelloWorldSpring {
         System.out.println(context.getBean("person3Parameters"));
         System.out.println(context.getBean("customBean"));
         System.out.println(context.getBean(Address.class));
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 
     }
 
