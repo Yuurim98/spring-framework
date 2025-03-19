b641128a281bb4593ee6d85dcccdac2004dce495
yurim <y1551463@gmail.com>
2025-03-19 12:34:48 +0900

    스프링 컨텍스트 생성
    스프링 컨텍스트 생성 및 빈 생성

---
+++ spring-framework/src/main/java/com/example/spring_framework/App02HelloWorldSpring.java
@@ -0,0 +1,17 @@
+package com.example.spring_framework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App02HelloWorldSpring {
+
+    public static void main(String[] args) {
+
+        // HelloWorldConfiguration 클래스를 기반으로 컨텍스트를 생성
+        var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        // 빈을 정의한 메서드명이 빈 이름이 됨
+        System.out.println(context.getBean("name"));
+
+    }
+
+}
