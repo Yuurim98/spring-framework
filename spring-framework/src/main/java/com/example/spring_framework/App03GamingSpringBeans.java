d09866f06af8cd0da564fc6e640e31e6baa47b07
yurim <y1551463@gmail.com>
2025-03-20 11:39:27 +0900

    Spring이 직접 빈을 생성하고 주입하도록 변경
    @Component 어노테이션을 클래스에 추가하여 자동으로 빈 등록
    @ComponentScan을 통해 특정 패키지 내의 @Component 클래스들을 찾아 자동으로 빈으로 등록
    생성자 주입을 활용하여 필요한 의존성을 자동으로 주입

--- spring-framework/src/main/java/com/example/spring_framework/App03GamingSpringBeans.java
+++ spring-framework/src/main/java/com/example/spring_framework/App03GamingSpringBeans.java
@@ -2,14 +2,17 @@ package com.example.spring_framework;
 
 import com.example.spring_framework.game.GameRunner;
 import com.example.spring_framework.game.GamingConsole;
-import com.example.spring_framework.game.PacmanGame;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("com.example.spring_framework.game") // 지정한 패키지에서 @Component 찾아 빈 등록
 public class App03GamingSpringBeans {
 
     public static void main(String[] args) {
 
-       var context = new AnnotationConfigApplicationContext(GamingConfiguration.class);
+       var context = new AnnotationConfigApplicationContext(App03GamingSpringBeans.class);
 
        context.getBean(GamingConsole.class).up();
        context.getBean(GameRunner.class).run();
