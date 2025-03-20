09f52c3bdb6194c6393db12548b03b074add876d
yurim <y1551463@gmail.com>
2025-03-20 13:24:08 +0900

    의존성 주입
    @Autowired (권장 X) : setter 주입
    @Autowired 어노테이션이 달려있으면 스프링이 자동으로 해당 메서드를 호출을 하고 매개변수로 필요한 의존성 객체를 스프링 빈에서 찾아 주입해준다

--- spring-framework/src/main/java/com/example/spring_framework/examples/a1/DepinjectionLauncherApplication.java
+++ spring-framework/src/main/java/com/example/spring_framework/examples/a1/DepinjectionLauncherApplication.java
@@ -9,11 +9,21 @@ import org.springframework.stereotype.Component;
 
 @Component
 class BusinessClass {
-    @Autowired
     Dependency1 dependency1;
 
-    @Autowired
     Dependency2 dependency2;
+
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("setter 주입 1");
+        this.dependency1 = dependency1;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("setter 주입 2");
+        this.dependency2 = dependency2;
+    }
 
     public String toString() {
         return "1 : " + dependency1 + " 2 : " + dependency2;
@@ -40,20 +50,24 @@ public class DepinjectionLauncherApplication {
        var context = new AnnotationConfigApplicationContext(DepinjectionLauncherApplication.class);
 
         Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-        // 클래스 만들기 전 : depinjectionLauncherApplication
+        // 1) 클래스 만들기 전 : depinjectionLauncherApplication
 
-        /* 클래스 만든 후 : 만든 클래스에 대한 빈이 생성됨
+        /* 2) 클래스 만든 후 : 만든 클래스에 대한 빈이 생성됨
          * depinjectionLauncherApplication
          * businessClass
          * dependency1
          * dependency2
          */
 
-        System.out.println(context.getBean(BusinessClass.class).toString());
-        // 의존성 주입 전 : 1 : null 2 : null
+        // System.out.println(context.getBean(BusinessClass.class).toString());
+        // 3) 의존성 주입 전 : 1 : null 2 : null
 
-        // @Autowired를 통해 필드 주입 후 :
+        // 4) @Autowired를 통해 필드 주입 후 :
         // 1 : com.example.spring_framework.examples.a1.Dependency1@4f67eb2a 2 : com.example.spring_framework.examples.a1.Dependency2@4a668b6e
+
+        System.out.println(context.getBean(BusinessClass.class).toString());
+        // 5) "setter 주입 1", "setter 주입 2" 출력
+
     }
 
 }
