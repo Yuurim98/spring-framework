e9044cb4aea0f8d40a978c6e463da9bbdc6603e4
yurim <y1551463@gmail.com>
2025-03-21 15:49:29 +0900

    @PostConstruct
    스프링 빈이 생성된 후 실행됨 (초기 설정 등의 초기화 로직에 사용)

---
+++ spring-framework/src/main/java/com/example/spring_framework/examples/e1/PrePostAnnotationsContextLauncherApplication.java
@@ -0,0 +1,49 @@
+package com.example.spring_framework.examples.e1;
+
+import com.example.spring_framework.examples.d1.BeanScopesLauncherApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.Arrays;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
+class SomeClass {
+
+    private SomeDependency someDependency;
+
+    public SomeClass(SomeDependency someDependency) {
+        this.someDependency = someDependency;
+        System.out.println("someDependency 주입");
+    }
+
+    @PostConstruct
+    public void initialize() {
+        someDependency.getReady();
+    }
+
+
+}
+
+@Component
+class SomeDependency {
+
+    public void getReady() {
+        System.out.println("SomeClass 빈 생성 후 바로 실행");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class PrePostAnnotationsContextLauncherApplication {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(
+            PrePostAnnotationsContextLauncherApplication.class);
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+    }
+
+}
