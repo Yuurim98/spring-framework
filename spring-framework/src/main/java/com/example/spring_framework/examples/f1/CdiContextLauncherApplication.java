e46b1c9a793a5145ea35b05c1113b183ee94c7b3
yurim <y1551463@gmail.com>
2025-03-21 16:14:35 +0900

    CDI (Contexts and Dependency Injection)
    Java EE 표준 DI 기술

---
+++ spring-framework/src/main/java/com/example/spring_framework/examples/f1/CdiContextLauncherApplication.java
@@ -0,0 +1,48 @@
+package com.example.spring_framework.examples.f1;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+//@Component
+@Named // Component를 대체
+class BusinessService {
+
+    private DataService dataService;
+
+    public DataService getDataService() {
+        return dataService;
+    }
+
+    //@Autowired
+    @Inject // Autowired를 대체
+    public void setDataService(DataService dataService) {
+        System.out.println("setter 주입");
+        this.dataService = dataService;
+    }
+}
+
+//@Component
+@Named
+class DataService {
+
+}
+
+@Configuration
+@ComponentScan
+public class CdiContextLauncherApplication {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(
+            CdiContextLauncherApplication.class);
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        System.out.println(context.getBean(BusinessService.class).getDataService());
+    }
+
+}
