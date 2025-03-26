ad561185b3a2a9fd6894f7dae994c7b58ac0f50a
yurim <y1551463@gmail.com>
2025-03-26 18:28:51 +0900

    XML 파일
    resources 디렉터리 하위에 XML 파일을 생성하여 스프링 컨텍스트와 빈을 정의할 수 있다.
    애플리케이션에서는 ClassPathXmlApplicationContext를 사용하여 해당 XML 파일을 로드하고, 설정된 빈을 컨테이너에 등록할 수 있다.

---
+++ spring-framework/src/main/java/com/example/spring_framework/examples/g1/XmlConfigurationContextLauncher.java
@@ -0,0 +1,17 @@
+package com.example.spring_framework.examples.g1;
+
+import java.util.Arrays;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class XmlConfigurationContextLauncher {
+
+    public static void main(String[] args) {
+        var context = new ClassPathXmlApplicationContext("contextConfiguration.xml");
+
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+
+        System.out.println(context.getBean("name")); // 홍길동
+        System.out.println(context.getBean("age")); // 20
+    }
+
+}
