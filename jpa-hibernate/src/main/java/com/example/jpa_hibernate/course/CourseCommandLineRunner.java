4576e4988d39e89a2e229c666691f5ae09350fbe
yurim <y1551463@gmail.com>
2025-03-27 14:21:19 +0900

    JPA로 변경

--- jpa-hibernate/src/main/java/com/example/jpa_hibernate/course/CourseCommandLineRunner.java
+++ jpa-hibernate/src/main/java/com/example/jpa_hibernate/course/CourseCommandLineRunner.java
@@ -1,15 +1,16 @@
-package com.example.jpa_hibernate.course.jdbc;
+package com.example.jpa_hibernate.course;
 
-import com.example.jpa_hibernate.course.Course;
+import com.example.jpa_hibernate.course.jdbc.CourseJdbcRepository;
+import com.example.jpa_hibernate.course.jpa.CourseJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-public class CourseJdbcCommandLineRunner implements CommandLineRunner {
+public class CourseCommandLineRunner implements CommandLineRunner {
 
     @Autowired
-    private CourseJdbcRepository repository;
+    private CourseJpaRepository repository;
 
 
     @Override
