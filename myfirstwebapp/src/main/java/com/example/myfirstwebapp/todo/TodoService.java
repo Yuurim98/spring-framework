0bf6a44412c7b000c5f0346be35bb1c0b616f7d1
yurim <y1551463@gmail.com>
2025-04-01 21:33:08 +0900

    시큐리티 추가

--- myfirstwebapp/src/main/java/com/example/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/src/main/java/com/example/myfirstwebapp/todo/TodoService.java
@@ -4,7 +4,6 @@ import jakarta.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
 import org.springframework.stereotype.Service;
 
 @Service
