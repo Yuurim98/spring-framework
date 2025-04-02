97f5529eaeb347dc94bc574ff9f33ebcad5a0e30
yurim <y1551463@gmail.com>
2025-04-02 13:28:40 +0900

    SecurityFilterChain 커스텀

--- myfirstwebapp/src/main/java/com/example/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/src/main/java/com/example/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -1,12 +1,16 @@
 package com.example.myfirstwebapp.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -18,6 +22,23 @@ public class SpringSecurityConfiguration {
         return new InMemoryUserDetailsManager(userDetails1, userDetails2);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+            auth -> auth.anyRequest().authenticated());
+        http.formLogin(withDefaults());
+        // http.csrf().disable(); Deprecated
+        http.csrf(csrf -> csrf.disable());
+        // http.headers().frameOptions().disable(); Deprecated
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     private UserDetails createNewUser(String username, String password) {
         UserDetails userDetails = User.builder()
             .passwordEncoder(input -> passwordEncoder().encode(input)) // 비밀번호 암호화
@@ -26,10 +47,5 @@ public class SpringSecurityConfiguration {
             .roles("USER", "ADMIN")
             .build();
         return userDetails;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
