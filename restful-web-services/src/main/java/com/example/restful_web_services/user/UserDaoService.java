7865d62682433bbfc43918446ad0c314495b37de
yurim <y1551463@gmail.com>
2025-04-04 12:43:34 +0900

    사용자 삭제

--- restful-web-services/src/main/java/com/example/restful_web_services/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/example/restful_web_services/user/UserDaoService.java
@@ -30,4 +30,8 @@ public class UserDaoService {
         users.add(user);
         return user;
     }
+
+    public void deleteById(int id) {
+        users.removeIf(user -> user.getId().equals(id));
+    }
 }
