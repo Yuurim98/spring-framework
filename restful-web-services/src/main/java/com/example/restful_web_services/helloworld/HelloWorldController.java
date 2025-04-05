56362ae0fd990e96c3385f26f6a215a18a8b60fe
yurim <y1551463@gmail.com>
2025-04-05 12:09:18 +0900

    Accept-Language 헤더 기반 다국어 처리

--- restful-web-services/src/main/java/com/example/restful_web_services/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/restful_web_services/helloworld/HelloWorldController.java
@@ -1,11 +1,20 @@
 package com.example.restful_web_services.helloworld;
 
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping("/hello-world")
     public String hello() {
@@ -20,5 +29,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloBean helloPathVariable(@PathVariable String name) {
         return new HelloBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping("/hello-internationalized")
+    public String helloInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("hello.message", null, "Default Message", locale);
     }
 }
