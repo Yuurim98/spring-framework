a309334ac4065cd247fbbc6cacb059d438e7a8a1
yurim <y1551463@gmail.com>
2025-04-08 17:27:06 +0900

    사용자 게시물 조회

--- restful-web-services/src/main/java/com/example/restful_web_services/user/UserJpaResource.java
+++ restful-web-services/src/main/java/com/example/restful_web_services/user/UserJpaResource.java
@@ -19,20 +19,20 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RestController
 public class UserJpaResource {
 
-    private final UserRepository repository;
+    private final UserRepository userRepository;
 
-    public UserJpaResource(UserRepository repository) {
-        this.repository = repository;
+    public UserJpaResource(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @GetMapping("/jpa/users")
     public List<User> retrieveAllUsers() {
-        return repository.findAll();
+        return userRepository.findAll();
     }
 
     @GetMapping("/jpa/users/{id}")
     public EntityModel<User> retrieveUser(@PathVariable int id) {
-        Optional<User> user = repository.findById(id);
+        Optional<User> user = userRepository.findById(id);
         if (user.isEmpty()) {
             throw new UserNotFoundException("id : " + id);
         }
@@ -45,7 +45,7 @@ public class UserJpaResource {
 
     @PostMapping("/jpa/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User savedUser = repository.save(user);
+        User savedUser = userRepository.save(user);
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
             .buildAndExpand(savedUser.getId()).toUri();
         return ResponseEntity.created(location).build();
@@ -53,6 +53,15 @@ public class UserJpaResource {
 
     @DeleteMapping("/jpa/users/{id}")
     public void deleteUser(@PathVariable int id) {
-        repository.deleteById(id);
+        userRepository.deleteById(id);
+    }
+
+    @GetMapping("/jpa/users/{id}/posts")
+    public List<Post> retrievePostsForUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isEmpty()) {
+            throw new UserNotFoundException("id : " + id);
+        }
+        return user.get().getPost();
     }
 }
