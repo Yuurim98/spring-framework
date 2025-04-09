9d3e3152f30d2ef0fd39d697efa7350fff0ab100
yurim <y1551463@gmail.com>
2025-04-09 12:21:52 +0900

    컴포넌트 생성
    클래스 컴포넌트는 현재 거의 사용되지 않음

--- frontend/todo-app/src/App.js
+++ frontend/todo-app/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <FirstComponent />
+            <SecondComponent />
+            <ThirdComponent />
+        </div>
+    );
+}
+
+function FirstComponent() {
+    return <div className="firstComponent">첫번째 컴포넌트</div>;
+}
+
+function SecondComponent() {
+    return <div className="secondComponent">두번째 컴포넌트</div>;
+}
+
+class ThirdComponent extends Component {
+    render() {
+        return <div className="thirdComponent">세번째 컴포넌트</div>;
+    }
 }
 
 export default App;
