389756943d29fa56d8122e11a1ef6fd7e332da32
yurim <y1551463@gmail.com>
2025-04-15 20:10:18 +0900

    로그인 하드코딩
    로그인 상태에 따라 메시지 출력

---
+++ frontend/todo-app/src/component/todo/TodoApp.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import "./TodoApp.css";
+
+export default function TodoApp() {
+    return (
+        <div className="TodoApp">
+            <LoginComponent />
+            {/* <WelcomeComponent /> */}
+        </div>
+    );
+}
+
+function LoginComponent() {
+    const [username, setUsername] = useState("test-name");
+    const [password, setPassword] = useState("");
+    const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+    const [showErrorMessage, setShowErrorMessage] = useState(false);
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleSubmit() {
+        console.log(username);
+        console.log(password);
+        if (username === "test-name" && password === "testpw") {
+            setShowSuccessMessage(true);
+            setShowErrorMessage(false);
+        } else {
+            setShowSuccessMessage(false);
+            setShowErrorMessage(true);
+        }
+    }
+
+    function SuccessMessageComponenet() {
+        if (showSuccessMessage) {
+            return <div className="successMessage">로그인 성공</div>;
+        }
+        return null;
+    }
+
+    function ErrorMessageComponenet() {
+        if (showErrorMessage) {
+            return <div className="errorMessage">로그인 실패</div>;
+        }
+        return null;
+    }
+
+    return (
+        <div className="Login">
+            <SuccessMessageComponenet />
+            <ErrorMessageComponenet />
+            <div className="LoginForm">
+                <div>
+                    <label>name: </label>
+                    <input
+                        type="text"
+                        name="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                    />
+                </div>
+                <div>
+                    <label>password: </label>
+                    <input
+                        type="password"
+                        name="password"
+                        value={password}
+                        onChange={handlePasswordChange}
+                    />
+                </div>
+                <div>
+                    <button type="button" name="login" onClick={handleSubmit}>
+                        login
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+// function WelcomeComponent() {
+//     return <div className="Welcome">Welcome 컴포넌트</div>;
+// }
