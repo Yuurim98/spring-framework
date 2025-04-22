ca84aa74d2031923e8a830ff9f8ccbd9c404cb53
yurim <y1551463@gmail.com>
2025-04-22 16:30:25 +0900

    Formik 라이브러리 활용하여 Todo 수정 폼 구현

--- frontend/todo-app/src/component/todo/TodoComponent.jsx
+++ frontend/todo-app/src/component/todo/TodoComponent.jsx
@@ -2,25 +2,69 @@ import { useParams } from "react-router-dom";
 import { useAuth } from "./security/AuthContext";
 import { retrieveTodoApi } from "./api/TodoApiService";
 import { useEffect, useState } from "react";
+import { Formik, Form, Field } from "formik";
 
 export default function TodoComponent() {
     const { id } = useParams();
     const authContext = useAuth();
     const username = authContext.username;
     const [description, setDescription] = useState("");
+    const [targetDate, setTargetDate] = useState("");
 
     useEffect(() => retrieveTodos(), [id]);
 
     function retrieveTodos() {
         retrieveTodoApi(username, id)
-            .then((response) => setDescription(response.data.description))
+            .then((response) => {
+                setDescription(response.data.description);
+                setTargetDate(response.data.targetDate);
+            })
             .catch((error) => console.log(error));
+    }
+
+    function onSubmit(values) {
+        console.log(values);
     }
 
     return (
         <div className="container">
             <h1>Todo</h1>
-            <div>description : {description}</div>
+            <div>
+                <Formik
+                    initialValues={{ description, targetDate }}
+                    enableReinitialize={true}
+                    onSubmit={onSubmit}
+                >
+                    {(props) => (
+                        <Form>
+                            <fieldse className="form-group">
+                                <label>Description</label>
+                                <Field
+                                    type="text"
+                                    className="form-control"
+                                    name="description"
+                                />
+                            </fieldse>
+                            <fieldse className="form-group">
+                                <label>Target Date</label>
+                                <Field
+                                    type="date"
+                                    className="form-control"
+                                    name="targetDate"
+                                />
+                            </fieldse>
+                            <div>
+                                <button
+                                    className="btn btn-success m-5"
+                                    type="submit"
+                                >
+                                    Save
+                                </button>
+                            </div>
+                        </Form>
+                    )}
+                </Formik>
+            </div>
         </div>
     );
 }
