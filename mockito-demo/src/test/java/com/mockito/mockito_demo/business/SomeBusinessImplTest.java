a125ed1d5590f7c3e4ab8bd256aedf1bfb678bd6
yurim <y1551463@gmail.com>
2025-04-25 20:11:39 +0900

    Stub 활용한 테스트 작성
    DataService가 변경되거나 테스트 시나리오가 추가될 때마다 Stub 수정 또는 생성이 필요

---
+++ mockito-demo/src/test/java/com/mockito/mockito_demo/business/SomeBusinessImplTest.java
@@ -0,0 +1,43 @@
+package com.mockito.mockito_demo.business;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SomeBusinessImplTest {
+
+    @Test
+    void findTheGreatestFromAllData_basicScenario() {
+        DataService dataServiceStub = new DataServiceStub1();
+        SomeBusinessImpl business = new SomeBusinessImpl(dataServiceStub);
+        int result = business.findTheGreatestFromAllData();
+
+        assertEquals(25, result);
+    }
+
+    @Test
+    void findTheGreatestFromAllData_withOneValue() {
+        DataService dataServiceStub = new DataServiceStub2();
+        SomeBusinessImpl business = new SomeBusinessImpl(dataServiceStub);
+        int result = business.findTheGreatestFromAllData();
+
+        assertEquals(15, result);
+    }
+
+}
+
+class DataServiceStub1 implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[] {25, 15, 5};
+    }
+}
+
+class DataServiceStub2 implements DataService {
+
+    @Override
+    public int[] retrieveAllData() {
+        return new int[] {15};
+    }
+}
