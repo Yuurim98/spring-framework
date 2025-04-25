b47180add384023dc30ad150e7fb22d70da44aba
yurim <y1551463@gmail.com>
2025-04-25 17:39:49 +0900

    엔티티 지정 및 더미 데이터

---
+++ 01-rest-api-starting-code/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into todo (id, description, done, target_date, username)
+values (10001, 'AWS 배우기', false, CURRENT_DATE(), 'test');
+
+insert into todo (id, description, done, target_date, username)
+values (10002, 'JPA 공부하기', false, CURRENT_DATE(), 'test');
+
+insert into todo (id, description, done, target_date, username)
+values (10003, '맛있는 거 먹기', false, CURRENT_DATE(), 'test');
