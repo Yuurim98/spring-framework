ec849500ce04e7caccf211b1d762a5ccd0e109d4
yurim <y1551463@gmail.com>
2025-04-27 15:19:14 +0900

    메모리에 사용자 설정

--- spring-security/src/main/java/com/example/spring_security/basic/BasicAuthSecurityConfiguration.java
+++ spring-security/src/main/java/com/example/spring_security/basic/BasicAuthSecurityConfiguration.java
@@ -6,6 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +26,20 @@ public class BasicAuthSecurityConfiguration {
         http.httpBasic(Customizer.withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
         return (SecurityFilterChain) http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("test")
+            .password("{noop}testpw")
+            .roles("USER")
+            .build();
+
+        UserDetails admin = User.withUsername("admin")
+            .password("{noop}testpw")
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
